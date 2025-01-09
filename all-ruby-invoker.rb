c85e346f367a709399110b91ce56b8cfa9a4ca44
Yusuke Endoh <mame@ruby-lang.org>
2025-01-09 11:11:41 +0900

    Run both parse.y and prism for Ruby 3.4 or later

--- all-ruby-invoker.rb
+++ all-ruby-invoker.rb
@@ -5,14 +5,23 @@ selector = /\/ruby-(1\.8|1\.9|2\.\d+|3\.\d+)/
 rubies = Dir.glob("/all-ruby/bin/ruby-*").sort
 rubies = rubies.grep_v(/preview|rc/)
 rubies = rubies.group_by {|f| f[selector, 1] }
-rubies = rubies.map do |ver, bins|
-  [ver, bins.max_by {|f| f.scan(/\d+/).map {|n| n.to_i } }]
-end.to_h
-rubies.delete(nil)
+h = {}
+rubies.each do |ver, bins|
+  next unless ver
+  bin = bins.max_by {|f| f.scan(/\d+/).map {|n| n.to_i } }
+  next unless bin
+  if (ver.split(".").map {|v| v.to_i } <=> [3, 4]) >= 0
+    h[ver + "+prism"] = [bin, "--parser=prism"]
+    h[ver + "+parse.y"] = [bin, "--parser=parse.y"]
+  else
+    h[ver] = [bin]
+  end
+end
+rubies = h
 inp = File.readable?("/inp") ? File.binread("/inp") : nil
 result = rubies.sort.map do |ver, bin|
   Thread.new do
-    out, err, status = Open3.capture3(bin, *ARGV, stdin_data: inp)
+    out, err, status = Open3.capture3(*bin, *ARGV, stdin_data: inp)
     [ver, out, err, status.exitstatus]
   end
 end.map {|th| th.value }
--- rubyfarm-invoker.rb
+++ rubyfarm-invoker.rb
@@ -5,8 +5,11 @@ out, err, status = Open3.capture3("ruby", "-v")
 out =~ /\((\d+-\d+-\d+[\w\d:]*) +\S+ +(\h+)\)/
 rev = "#$2 (#$1)"
 inp = File.readable?("/inp") ? File.binread("/inp") : nil
-out, err, status = Open3.capture3("ruby", *ARGV, stdin_data: inp)
-result = Marshal.dump([[rev, out, err, status.exitstatus]])
+result = ["prism", "parse.y"].map do |pa|
+  out, err, status = Open3.capture3("ruby", "--parser=" + pa, *ARGV, stdin_data: inp)
+  ["#{ rev }+#{ pa }", out, err, status.exitstatus]
+end
+result = Marshal.dump(result)
 puts "ok:#{ result.size }"
 print result
 $stdout.close
